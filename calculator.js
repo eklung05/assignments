cfbb9858340f4e0cb93fdb3551dd6721f5e28f88
eklung05 <eklung05@gmail.com>
2022-01-15 17:34:37 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,19 @@
+//define function multiply
+function multiply(a,b) {
+	return a*b
+}
+
+//define function add
+function add(a,b) {
+	return a+b
+}
+
+//define function subtract
+function subtract(a,b) {
+	return a-b
+}
+
+//define function divide
+function divide(a,b) {
+	return a/b
+}
