84455d02056b6d8f867ed3eeb4ecda5602cbd6a2
eklung05 <eklung05@gmail.com>
2022-01-15 17:40:22 -0500

    add code to call function and demonstrate multiply

---
+++ calculator.js
@@ -0,0 +1,24 @@
+//define function multiply
+function multiply(a,b) {
+	return a*b
+}
+
+//define function add
+function add(a,b) {
+	return a+b
+}
+
+//define function subtract
+function subtract(a,b) {
+	return a-b
+}
+
+//define function divide
+function divide(a,b) {
+	return a/b
+}
+
+//call the function and print a line explaining what is happening.
+console.log("I'm going to use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
